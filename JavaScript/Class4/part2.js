626def7e55a6c62641c0306103d9cae8ac4a7da7
Manik Maity <manikmaity156@gmail.com>
2023-11-19 10:52:20 +0530

    Javascript class 3, class 4

---
+++ JavaScript/Class4/part2.js
@@ -0,0 +1,44 @@
+// let email = "m@gmail.com";
+// console.log(Boolean(email))
+// if (email){
+//     console.log("Password");
+// }
+// else{
+//     console.log("Email please")
+// }
+
+// truthy and falsy value
+
+//falsy value : false, 0, "", null, undefined, NaN,
+
+// let email = "manik@gmail.com", pass = "dhh455";
+// if (email && pass){
+//     console.log("You are logged in");
+// }
+// else{
+//     if (email){
+//         console.log("Enter password");
+//     }
+//     else if (pass){
+//         console.log("Enter email");
+//     }
+//     else{
+//         console.log("Enter email and password")
+//     }
+// }
+
+// logical operator - &&, ||
+let googleToken = "m@gmail.com";
+let githubToken = "manik@github.com";
+
+if (googleToken || githubToken ){
+    if (googleToken){
+        console.log("Logged in by google");
+    }
+    else (
+        console.log("Logged in with github")
+    )
+}
+else {
+    console.log("Login using google or github")
+}
