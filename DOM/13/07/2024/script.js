db74081d1847c8d614812507d83585547b59f5a2
Manik Maity <manikmaity156@gmail.com>
2024-08-11 22:47:13 +0530

    Done DOM class - 13/07/2024

--- DOM/13/07/2024/script.js
+++ DOM/13/07/2024/script.js
@@ -1,16 +1,41 @@
 const inputBox = document.getElementById("tier");
 const submitBtnEle = document.getElementById("submit");
 const tierSection = document.querySelector(".tier-list-section");
+const imageForm = document.getElementById("image-form")
+const imageInputBox = document.getElementById("image-input");
+const nonTierImageContainer = document.querySelector(".non-tier-section");
 
 submitBtnEle.addEventListener("click", (e) => {
+    e.preventDefault();
     if (inputBox.value == ""){
         alert("Please enter the value");
         return;
     }
-    e.preventDefault();
     tierSection.appendChild(createTierList(inputBox.value));
     inputBox.value = "";
 })
+
+imageForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const imageUrl = imageInputBox.value;
+    imageInputBox.value = "";
+    if(imageUrl.trim() == ""){
+        alert("Please enter a valid image.")
+        return;
+    }
+    const newImageDiv = createTierImageItem(imageUrl);
+    nonTierImageContainer.appendChild(newImageDiv);
+
+})
+
+function createTierImageItem (txt = ""){
+    const imageDiv = document.createElement("div");
+    imageDiv.classList.add("item-container");
+    const img = document.createElement("img");
+    img.src = txt;
+    imageDiv.appendChild(img);
+    return imageDiv;
+}
 
 
 function createTierList (txt = ""){
